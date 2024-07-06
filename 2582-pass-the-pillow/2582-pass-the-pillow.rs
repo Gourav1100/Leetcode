f177ddd9faf5618646ad2d573ff28b96f0f0aeee
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2024-07-06 21:51:59 +0530

    Time: 0 ms (100.00%), Space: 2 MB (50.00%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.rs
+++ 2582-pass-the-pillow/2582-pass-the-pillow.rs
@@ -1,7 +1,5 @@
 impl Solution {
     pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
-        let round_trip = 2 * (n - 1);
-        let remainder = time % round_trip;
-        if remainder > n - 1 {2 * n - remainder - 1} else {remainder + 1}
+        if time % (2 * (n - 1)) > n - 1 {2 * n - (time % (2 * (n - 1))) - 1} else {(time % (2 * (n - 1))) + 1}
     }
 }
