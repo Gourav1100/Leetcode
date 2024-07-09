2939d00483e8ad34308547c905b244c0806927e8
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2024-07-09 11:34:05 +0530

    Time: 18 ms (100.00%), Space: 8.5 MB (100.00%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.rs
+++ 1701-average-waiting-time/1701-average-waiting-time.rs
@@ -1,13 +1,11 @@
 impl Solution {
     pub fn average_waiting_time(customers: Vec<Vec<i32>>) -> f64 {
-        use std::cmp;
         let mut wait: f64 = 0.0;
         let n = customers.len() as f64;
         let mut next_time: i32 = customers[0][0];
         for customer in customers {
-            next_time = cmp::max(next_time, customer[0]) + customer[1];
+            next_time = if next_time > customer[0] {next_time} else {customer[0]} + customer[1];
             wait += (next_time - customer[0]) as f64;
-            // println!("order_at - {}, wait - {}", next_time, current_wait);
         }
         wait / n
     }
