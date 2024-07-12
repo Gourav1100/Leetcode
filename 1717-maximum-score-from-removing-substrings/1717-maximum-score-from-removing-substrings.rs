b314b2ac3681892424d7ad38dc1b7cf9efc1282f
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2024-07-13 01:23:01 +0530

    Time: 15 ms (50.00%), Space: 2.6 MB (25.00%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.rs
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.rs
@@ -1,29 +1,26 @@
 impl Solution {
     pub fn maximum_gain(mut s: String, x: i32, y: i32) -> i32 {
-        let (max, min, maximizeString, minimizeString) = if x > y {
-            (x, y, "ab".to_string(), "ba".to_string())
+        let (max, min, maximizeStringFirst, maximizeStringSecond, minimizeStringFirst, minimizeStringSecond) = if x > y {
+            (x, y, 'a', 'b', 'b', 'a')
         } else {
-            (y, x, "ba".to_string(), "ab".to_string())
+            (y, x, 'b', 'a', 'a', 'b')
         };
         let mut stack: Vec<char> = Vec::new();
         let mut res = 0;
         for ch in s.chars() {
             let last = (stack.len() - 1) as i32;
-            if last >= 0 && stack[last as usize] == maximizeString.chars().nth(0).unwrap() && ch == maximizeString.chars().nth(1).unwrap() {
+            if last >= 0 && stack[last as usize] == maximizeStringFirst && ch == maximizeStringSecond {
                 res += max;
                 stack.pop();
             } else {
                 stack.push(ch);
             }
         }
-        s = "".to_string();
-        for ch in stack {
-            s += ch.to_string().as_str().as_ref();
-        }
-        stack = Vec::new();
+        s = stack.iter().collect();
+        stack.clear();
         for ch in s.chars() {
             let last = (stack.len() - 1) as i32;
-            if last >= 0 && stack[last as usize] == minimizeString.chars().nth(0).unwrap() && ch == minimizeString.chars().nth(1).unwrap() {
+            if last >= 0 && stack[last as usize] == minimizeStringFirst && ch == minimizeStringSecond {
                 res += min;
                 stack.pop();
             } else {
