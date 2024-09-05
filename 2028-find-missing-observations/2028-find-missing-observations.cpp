13559149a7274061e364f24227232bc8b234be17
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2024-09-05 23:40:05 +0530

    Time: 91 ms (80.39%), Space: 116.2 MB (86.03%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -5,13 +5,13 @@ public:
         while(m > 0) {
             sum -= rolls[--m];
         }
-        int quo = sum / n, rem = sum % n, index = 0;
+        int quo = sum / n, rem = sum % n;
         if (quo > 6 || quo <= 0 || (quo == 6 && rem != 0)) {
             return {};
         }
         vector<int> result(n, quo);
         while(rem != 0) {
-            result[index++]++;
+            result[m++]++;
             rem--;
         }
         return result;
