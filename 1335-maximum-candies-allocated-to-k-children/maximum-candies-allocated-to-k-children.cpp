4713597c25f80c043f26c3a9c25d9f947483c1f1
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2025-03-14 14:00:10 +0530

    Time: 33 ms (37.17%) | Memory: 88.3 MB (7.85%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
         int front = 1, end = 10e7;
-        long long count = 0;
         while(front <= end) {
             int mid = end - ((end - front) >> 1);
-            count = 0;
+            long long count = 0;
             for (int i = 0; i < candies.size(); i++) {
                 count += candies[i] / mid;
             }
-            if (count >= k) {
-                front = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            front = count >= k ? mid + 1 : front;
+            end = count >= k ? end : mid - 1;
         }
         return end;
     }
