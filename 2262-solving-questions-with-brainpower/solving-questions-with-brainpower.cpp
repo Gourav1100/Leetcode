1e5ee0df24442a88282cd04f1cb70275e091eef1
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2025-04-01 10:33:26 +0530

    Time: 11 ms (32.59%) | Memory: 119.5 MB (95.63%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> result(n, 0);
+        long long *result = new long long[n];
         for(int i = n - 1; i >= 0; i--) {
             long long next = i + questions[i][1] + 1 < n ? result[i + questions[i][1] + 1] : 0;
             result[i] = max((long long)questions[i][0] + next, i + 1 < n ? (long long)result[i + 1] : 0);
