b462e514e73b3db4c15b57224316ccebc9b82578
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2025-04-14 23:09:28 +0530

    Time: 5 ms (87.24%) | Memory: 11 MB (71.62%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -4,12 +4,10 @@ public:
         int n = arr.size(), res = 0;
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
-                if (abs(arr[i] - arr[j]) <= a) {
-                    for(int k = j + 1; k < n; k++) {
-                        if (abs(arr[j] - arr[k]) <= b && abs(arr[k] - arr[i]) <= c) {
-                            res++;
-                        }
-                    }
+                if (abs(arr[i] - arr[j]) > a) continue;
+                for(int k = j + 1; k < n; k++) {
+                    if (abs(arr[j] - arr[k]) > b || abs(arr[k] - arr[i]) > c) continue;
+                    res++;
                 }
             }
         }
