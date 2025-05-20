390f6967e9b3605380c109c6f0559a2876fea484
Gourav Bidhuri <77623755+Gourav1100@users.noreply.github.com>
2025-05-20 11:35:11 +0530

    Time: 16 ms (42.95%) | Memory: 290.8 MB (98.83%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int temp[100001] = {};
-        int tempp[100001] = {};
+        int temp[100001] = {}, tempp[100001] = {}, sum = 0, n = nums.size();
         for(auto& query: queries) {
             temp[query[0]]++;
             tempp[query[1]]--;
         }
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < n; i++) {
             if (temp[i] > 0) {
                 sum += temp[i];
             }
